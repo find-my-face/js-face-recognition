2e7e4e2233682a842beb1ddcd034078bf34c2d8f
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-07-15 16:23:35 +0100

    Set up storage

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,31 @@
 const functions = require("firebase-functions");
 // const gcs = require("@google-cloud/storage")();
+const tfjs = require("@tensorflow/tfjs-node");
+const canvas = require("canvas");
+const { Canvas, Image, ImageData } = canvas;
 const os = require("os");
+const fs = require("fs");
 const path = require("path");
 const spawn = require("child-process-promise").spawn;
+const faceapi = require("face-api.js");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange = functions.storage.object().onFinalize(event => {
-  const object = event.data;
-  const bucket = admin.storage().bucket();
+
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+const MODEL_DIR = (require = "./models");
+
+exports.onFileChange = functions.storage.object().onFinalize(async object => {
+  // const object = event.data;
+  const fileBucket = object.bucket;
+  const bucket = admin.storage().bucket(fileBucket);
   const contentType = object.contentType;
   const filePath = object.name;
-  console.log(event);
   console.log("File change detected, function execution started");
+  console.log(object);
 
   if (object.resourceState === "not_exists") {
     console.log("We deleted a file, exit...");
@@ -27,21 +37,48 @@ exports.onFileChange = functions.storage.object().onFinalize(event => {
     return;
   }
 
-  const destBucket = gcs.bucket(bucket);
+  if (!filePath.startsWith("Users")) {
+    return console.log("Not an upload from User");
+  }
+
   const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+  const tmpPhotographerFilePath = path.join(
+    os.tmpdir(),
+    path.basename(`${filePath}/Photographers`)
+  );
   const metadata = { contentType: contentType };
-  return destBucket
+
+  return bucket
     .file(filePath)
     .download({
       destination: tmpFilePath
     })
     .then(() => {
+      //set ^^ to a known const
+      //set photographerfilepath to const unknown
       return spawn("convert", [tmpFilePath, "-resize", "500x500", tmpFilePath]);
+      //do facial recognition stuff
+      //get selfie --triggered from new add to users known
+      //get the photographers albums unknown
+      //compare... map and compare to the known
+      //a folder of matched photos?
     })
     .then(() => {
-      return destBucket.upload(tmpFilePath, {
+      //return the FOLDER of matches images
+      return bucket.upload(tmpFilePath, {
         destination: "resized-" + path.basename(filePath),
         metadata: metadata
       });
     });
+});
+
+exports.processSelfie = functions.storage.object().onFinalize(async object => {
+  const fileBucket = object.bucket;
+  const bucket = admin.storage().bucket(fileBucket);
+  const contentType = object.contentType;
+  const filePath = object.name;
+
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_DIR);
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_DIR);
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_DIR);
 });
